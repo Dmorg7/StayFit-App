04dcc3510a774206a809f4859f6a5d1dc284f657
Benjamin Daniel Morgan <benjamindanielmorgan@benjamins-mbp.home>
2020-02-12 16:51:26 +0000

    created fitness app project, created a buttom navigation bar, created 4 pages to navigate between, started the registration and log in validation

---
+++ lib/workout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WorkoutPage extends StatelessWidget {
+  @override 
+    Widget build(BuildContext context) {
+      return Container(
+        padding: EdgeInsets.all(25.0),
+        child: Text('Workout', style: TextStyle(fontSize: 36.0),),
+      );
+    }
+}
